f6df83fabb28493e4d683b16f86089eab50c9bb9
Frances Brown <57534807+frannyyb@users.noreply.github.com>
2025-11-12 17:15:28 -0800

    Changed Test Data Again

--- app.js
+++ app.js
@@ -81,14 +81,7 @@ const CONFIG = {
 // Mock data generator for testing
 const generateMockData = () => {
   const allTeamNames = [
-    'JCM Noah', 'Peter Gay', 'Franny Pack', 'David Henriquez',
-    'Ellie Jones', 'Eloe Gill-Williams', 'Felipe Riberio', 'Fran Brown',
-    'Galen Kary', 'Gelareh Dehnad', 'Jacobi Mehringer', 'Jane Monaghan',
-    'Jared Randle', 'Jason Strickland', 'Jojo Ball', 'Jovan Lim & Priya Moorthy',
-    'Kacey Kelley', 'Laura Wood', 'Lauren Hill Vaughan', 'Lindsay Varquez',
-    'Maisie Plew', 'Mariah Mercier', 'MJ', 'Molly Dyson',
-    'Nai Lucifora', 'Paige Fitzmaurice', 'Paris Fontes-Michel', 
-    'Siobhan Robinson', 'Will Curtis'
+    'JCM Noah'
   ];
   
   const teamData = {};
